ce1d83c31f51f334393a0b431b43335a033a1583
alekshoxhaer4786 <146690878+alekshoxhaer4786@users.noreply.github.com>
2025-07-06 02:36:21 -0600

    Update build.rs

--- coprocessor/fhevm-engine/gw-listener/build.rs
+++ coprocessor/fhevm-engine/gw-listener/build.rs
@@ -1,5 +1,4 @@
 use std::path::Path;
-
 use foundry_compilers::{
     multi::MultiCompiler,
     solc::{Solc, SolcCompiler},
@@ -9,7 +8,6 @@ use semver::Version;
 
 fn main() {
     let paths = ProjectPathsConfig::hardhat(Path::new(env!("CARGO_MANIFEST_DIR"))).unwrap();
-    // Use a specific version due to an issue with libc and libstdc++ in the rust Docker image we use to run it.
     let solc = Solc::find_or_install(&Version::new(0, 8, 28)).unwrap();
     let project = Project::builder()
         .paths(paths)
